470aa962311b86b9caf3cf793d69ba9dd1776724
taofeek-ob <taofeek01@yahoo.com>
2022-10-27 14:16:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+            lerp: 0.13,
+            smartphone: {smooth: true},
+            tablet: {smooth: true}
+
+}
+  )
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+scroller.on('scroll', ScrollTrigger.update)
+
+ScrollTrigger.scrollerProxy(
+    '.container', {
+        scrollTop(value) {
+            return arguments.length ?
+            scroller.scrollTo(value, 0, 0) :
+            scroller.scroll.instance.scroll.y
+        },
+        getBoundingClientRect() {
+            return {
+                left: 0, top: 0, 
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        }
+    }
+)
+
+
+ScrollTrigger.create({
+    trigger: '.image-mask',
+    scroller: '.container',
+    start: 'top+=30% 50%',
+    end: 'bottom-=40% 50%',
+    animation: gsap.to('.image-mask', {backgroundSize: '120%'}),
+    scrub: 2,
+    // markers: true
+})
+
+
+ScrollTrigger.addEventListener('refresh', () => scroller.update())
+// ScrollTrigger.normalizeScroll(true);
+
+ScrollTrigger.refresh()
